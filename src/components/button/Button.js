bddf706bd4e7fc8c393c59831e72db9925ac6e44
TatShen <shenets-97@mail.ru>
2022-11-04 21:28:58 +0300

    add count

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,20 +1,54 @@
 import { Component } from "../../core";
+import "./button.scss";
 
-export class Button extends Component{
-    constructor(){
-        super();
-        this.state = {
-            isActive: false
-        }
-    }
-
-    render(){
-        return `
-            <button>Click</button>
-        `
+export class Button extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 1,
     };
+  }
 
-    
+  minus() {
+    this.setState((state) => {
+      return {
+        ...state,
+        count: state.count >0 ? --state.count: state.count  
+        }
+      }
+    )
+  }
+
+  plus() {
+    this.setState((state) => {
+      return {
+        ...state,
+        count: ++state.count,
+      };
+    });
+  }
+
+  registerEvents() {
+    this.addEventListener("click", (evt) => {
+      if (evt.target.closest(".plus")) {
+        this.plus();
+      } else if (evt.target.closest(".minus")) {
+        this.minus();
+      }
+    });
+  }
+
+  render() {
+    return `
+            <button class='plus'>
+                +
+            </button>
+            <span> ${this.state.count}</span>
+            <button class='minus'>
+                -
+            </button>
+        `;
+  }
 }
 
-customElements.define('my-button', Button)
+customElements.define("my-button", Button);
